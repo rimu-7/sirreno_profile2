068b3bae75311da285f1d066fa0484f80b44f8ff
rimu-7 <rrimu99@gmail.com>
2025-02-19 22:51:15 +0600

    Quote & artist name

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Carousel from "./Carousel";
 import Carousel2 from "./Carousel2";
 import Navbar from "../../Shared/Nav/Components/Navbar";
 
+const quote = [
+  {
+    id: 1,
+    description: `“I’m all you need baby” `,
+  },
+  {
+    id: 2,
+    description: `“Looking into my eyes when I’m talking to you, take this love that I’m giving to you “ `,
+  },
+  {
+    id: 3,
+    description: `“Everyday is holiday, I will celebrate, bad energy for away”`,
+  },
+  {
+    id: 4,
+    description: `“No money no love”`,
+  },
+  {
+    id: 5,
+    description: `“Never been the same since I met you”`,
+  },
+  {
+    id: 6,
+    description: `“Fine like Benita, sweet like Belinda”`,
+  },
+  {
+    id: 7,
+    description: `“She do me something I can’t deny”`,
+  },
+];
+
 function Home() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % quote.length);
+    }, 5000); // Change quote every 5 seconds
+
+    return () => clearInterval(interval); // Cleanup on unmount
+  }, [quote.length]);
   return (
     <div
       className="h-screen min-w-screen bg-no-repeat 
@@ -18,8 +58,11 @@ function Home() {
       <Navbar />
 
       {/* Artist Name in Top Right Corner */}
-      <div className="absolute top-20 sm:top-16 lg:top-30 right-auto sm:right-20 text-5xl">
-        artist name
+      <div
+        style={{ fontFamily: "Paytone One, serif" }}
+        className="absolute top-20 sm:top-16 lg:top-30 right-auto sm:right-20 text-5xl"
+      >
+        JSlught
       </div>
 
       {/* Carousel */}
@@ -29,8 +72,8 @@ function Home() {
       </div>
 
       {/* Quote in Bottom Right Corner */}
-      <div className="absolute bottom-10 px-16 italic right-4 text-lg">
-        <p>artist quote</p>
+      <div className="absolute bottom-20 px-16 italic right-4 text-lg">
+        <p >{quote[currentIndex].description}</p>
       </div>
     </div>
   );
