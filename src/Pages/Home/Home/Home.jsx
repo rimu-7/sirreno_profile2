7323a024e295fac48a5b3336aa51254497427c28
rimu-7 <rrimu99@gmail.com>
2025-02-19 23:14:21 +0600

    home

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,52 +1,65 @@
 import React, { useEffect, useState } from "react";
+import { useSpring, animated } from "react-spring";
 import Carousel from "./Carousel";
 import Carousel2 from "./Carousel2";
-import Navbar from "../../Shared/Nav/Components/Navbar";
+import NavbarPC from "../../Shared/Nav/Components/Navbar";
+import Navbar from "../../Shared/Navbar/Navbar";
 
 const quote = [
   {
     id: 1,
-    description: `“I’m all you need baby” `,
+    description: `"I'm all you need baby" `,
   },
   {
     id: 2,
-    description: `“Looking into my eyes when I’m talking to you, take this love that I’m giving to you “ `,
+    description: `"Looking into my eyes when I'm talking to you, take this love that I'm giving to you " `,
   },
   {
     id: 3,
-    description: `“Everyday is holiday, I will celebrate, bad energy for away”`,
+    description: `"Everyday is holiday, I will celebrate, bad energy for away"`,
   },
   {
     id: 4,
-    description: `“No money no love”`,
+    description: `"No money no love"`,
   },
   {
     id: 5,
-    description: `“Never been the same since I met you”`,
+    description: `"Never been the same since I met you"`,
   },
   {
     id: 6,
-    description: `“Fine like Benita, sweet like Belinda”`,
+    description: `"Fine like Benita, sweet like Belinda"`,
   },
   {
     id: 7,
-    description: `“She do me something I can’t deny”`,
+    description: `"She do me something I can't deny"`,
   },
 ];
 
 function Home() {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [quoteAnimation, setQuoteAnimation] = useSpring(() => ({
+    from: { transform: "translateY(100%)" },
+    to: { transform: "translateY(0%)" },
+    config: { duration: 1000 },
+  }));
 
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % quote.length);
-    }, 5000); // Change quote every 5 seconds
+      setQuoteAnimation((prev) => ({
+        ...prev,
+        from: { transform: "translateY(100%)" },
+        to: { transform: "translateY(0%)" },
+      }));
+    }, 7000); // Change quote every 7 seconds
 
     return () => clearInterval(interval); // Cleanup on unmount
   }, [quote.length]);
+
   return (
     <div
-      className="h-screen min-w-screen bg-no-repeat 
+      className="min-h-screen min-w-screen bg-no-repeat 
       flex items-center justify-center w-full relative
       text-center text-2xl text-white"
     >
@@ -55,9 +68,18 @@ function Home() {
         className="absolute inset-0 w-full h-full overflow-hidden bg-cover 
         bg-[url(https://res.cloudinary.com/ddssf6cm6/image/upload/v1739851698/artist_website_1_fe02ec.jpg)]"
       ></div>
-      <Navbar />
 
-      {/* Artist Name in Top Right Corner */}
+      {/* Navbar */}
+      <div className="absolute top-0 left-0 w-full">
+        <div className="hidden sm:block">
+          <NavbarPC />
+        </div>
+        <div className="block sm:hidden">
+          <Navbar />
+        </div>
+      </div>
+
+      {/* Artist Name */}
       <div
         style={{ fontFamily: "Paytone One, serif" }}
         className="absolute top-20 sm:top-16 lg:top-30 right-auto sm:right-20 text-5xl"
@@ -70,11 +92,18 @@ function Home() {
         <Carousel />
         <Carousel2 />
       </div>
-
-      {/* Quote in Bottom Right Corner */}
-      <div className="absolute bottom-20 px-16 italic right-4 text-lg">
-        <p >{quote[currentIndex].description}</p>
+      <div className="block sm:hidden px-4 w-full h-48 justify-between">
+        <Carousel />
+        <Carousel2 />
       </div>
+
+      {/* Quote */}
+      <animated.div
+        style={quoteAnimation}
+        className="absolute bottom-20 px-4 sm:px-16 italic right-4 text-lg"
+      >
+        <p>{quote[currentIndex].description}</p>
+      </animated.div>
     </div>
   );
 }
